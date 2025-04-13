93686db581f505430d8aefba3618a064e199f957
MichaelC609 <mjcastillo@cpp.edu>
2025-04-13 14:44:22 -0700

    added navbar html

---
+++ frontend/cycling-app/src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+function Navbar()
+{
+    return(
+        <>
+            <nav className="nav">
+                <a href="/" className="site-title">Biking App</a>
+                <ul>
+                    <li>
+                        <a href="/home">Home</a>
+                    </li>
+                    <li>
+                        <a href="/route-optimizer">Optimize Your <br />Routes</a>
+                    </li>
+                    <li>
+                        <a href="/performance-tracking">Track Your <br />Performance</a>
+                    </li>
+                    <li>
+                        <a href="/login">Login</a>
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+}
+
+export default Navbar;
