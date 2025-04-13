de3a9f980493396db5cade486fed1bae76bbe666
MichaelC609 <mjcastillo@cpp.edu>
2025-04-13 11:02:46 -0700

    resolved routing issues

--- frontend/cycling-app/src/App.js
+++ frontend/cycling-app/src/App.js
@@ -1,7 +1,9 @@
+import LandingPage from './pages/LandingPage.jsx';
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <LandingPage />
     </>
   );
 }
--- frontend/cycling-app/src/index.js
+++ frontend/cycling-app/src/index.js
@@ -1,12 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-const router = createBrowserRouter([
-  {path: '/', element: <App />}
-]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +8,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
