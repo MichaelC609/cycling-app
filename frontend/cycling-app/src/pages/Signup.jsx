fc533916c7a713322beb99425ba3a48e6b05b17f
MichaelC609 <mjcastillo@cpp.edu>
2025-04-14 00:25:21 -0700

    created signup page

--- frontend/cycling-app/src/pages/Signup.jsx
+++ frontend/cycling-app/src/pages/Signup.jsx
@@ -1,11 +1,33 @@
-import Navbar from "../components/Navbar";
+import '../static/signup.css';
 
 function Signup()
 {
     return(
         <>
-            <Navbar />
-            <h1>Signup for an account</h1>
+            <div className="container">
+                <div className="header">
+                    <div className="text">Sign Up</div>
+                    <div className="underline"></div>    
+                </div>
+                <div className="inputs">
+                    <div className="input">
+                        <input type="text" placeholder='Enter First and Last Name'/>
+                    </div>
+                    <div className="input">
+                        <input type="email" placeholder='Enter Email'/>
+                    </div>
+                    <div className="input">
+                        <input type="password" placeholder='Enter a Password'/>
+                    </div>
+                </div>  
+                <div className="forgot-password">Forgot Password? 
+                    <span> Click Here</span>
+                </div>
+                <div className="submit-container">
+                    <div className="submit">Sign Up</div>
+                    <div className="submit">Log In</div>
+                </div>  
+            </div>
         </>
     )
 }
